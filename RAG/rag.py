09de9b325c380639ff7866f4805ddd14f4d6381e
Kyle Java <java002@cougars.csusm.edu>
2024-06-28 09:18:22 -0700

    Update rag.py

--- RAG/rag.py
+++ RAG/rag.py
@@ -12,8 +12,8 @@
 # In[13]:
 
 
-# Data handling 
-import pandas as pd 
+# Data handling
+import pandas as pd
 import json
 
 # Database stuff
@@ -30,15 +30,15 @@ from sentence_transformers import SentenceTransformer
 # In[14]:
 
 
-# Load in data
-with open('package_info.json', 'r') as json_file:
-    data = json.load(json_file)
-
-df = pd.DataFrame(data)
-df = df[['name', 'key_features', 'description']]
-df['key_features'] = df['key_features'].apply(lambda x: ', '.join(map(str, x)))
-df['all_info'] = df.apply(lambda row: str(row.to_dict()), axis=1)
-
+# # Load in data
+# with open('package_info.json', 'r') as json_file:
+#     data = json.load(json_file)
+#
+# df = pd.DataFrame(data)
+# df = df[['name', 'key_features', 'description']]
+# df['key_features'] = df['key_features'].apply(lambda x: ', '.join(map(str, x)))
+# df['all_info'] = df.apply(lambda row: str(row.to_dict()), axis=1)
+#
 
 # # Part 2: Populate VectorDB
 
@@ -52,11 +52,11 @@ model_name = "sentence-transformers/all-MiniLM-L6-v2"
 class Model():
     def __init__(self, model_name):
         self.model = SentenceTransformer(model_name)
-    
+
     def embed_documents(self, text):
         embeddings = self.model.encode(text, convert_to_numpy=True).tolist()
         return embeddings
-    
+
     def embed_query(self, text):
         text = text.lower().strip()
         embeddings = self.model.encode(text, convert_to_numpy=True).tolist()
@@ -65,12 +65,12 @@ class Model():
 
 # In[16]:
 
-
-# Create the data loader
-loader = DataFrameLoader(df, page_content_column="all_info")
-data = loader.load()
-db_size = len(data)
-print(db_size)
+#
+# # Create the data loader
+# loader = DataFrameLoader(df, page_content_column="all_info")
+# data = loader.load()
+# db_size = len(data)
+# print(db_size)
 
 
 # In[18]:
@@ -106,70 +106,70 @@ def search(queries, n, sim_search, mmr_search):
     if (n > db_size):
         print(f"Error: n is too large, there are only {db_size} documents in the database.")
         return None
-    
+
     if not isinstance(queries, list):
         print("Error: Input must be a list of strings.")
         return None
-    
+
     fetch_k = max(int(db_size/10), n)
-    
+
     sim_results = []
     mmr_results = []
     positions = {}
     packages = {}
-    
-    
+
+
     for query in queries:
         sim_packages = sim_search(query=query, k=n)
         mmr_packages = mmr_search(query=query, k=min(fetch_k, n), fetch_k=fetch_k, alpha=0.5)
-        
+
         sim_names = [p[0].metadata['name'] for p in sim_packages]
         mmr_names = [p.metadata['name'] for p in mmr_packages]
-        
+
         num_sim = len(sim_names)
         num_mmr = len(mmr_names)
-        
+
         sim_results.append(sim_names)
         mmr_results.append(mmr_names)
-        
+
         for p in sim_packages:
             name = p[0].metadata['name']
             if name in positions:
                 continue
             packages[name] = produce_dict(p[0])
             positions[name] = 0
-        
+
         for p in mmr_packages:
             name = p.metadata['name']
-            if name in positions: 
+            if name in positions:
                 continue
             packages[name] = produce_dict(p)
             positions[name] = 0
-           
-     
+
+
     all = [key for key in positions]
     for iter in range(len(sim_results)):
         names_left = all.copy()
         for ind, name in enumerate(sim_results[iter]):
             positions[name] += ind+1
-            names_left.remove(name)   
-        
+            names_left.remove(name)
+
         for name in names_left:
             positions[name] += num_sim+1
-        
+
         names_left = all.copy()
         for ind, name in enumerate(mmr_results[iter]):
             positions[name] += ind+1
-            names_left.remove(name)   
-        
+            names_left.remove(name)
+
         for name in names_left:
-            positions[name] += num_mmr+1        
-            
+            positions[name] += num_mmr+1
+
     for key in positions:
         packages[key]['Average Position'] = positions[key]/(2*len(queries))
-        
+
     packages = dict(sorted(packages.items(), key=lambda item: item[1]['Average Position']))
-     
+
     return [packages[key] for key in packages]
 
 
@@ -179,17 +179,13 @@ def search(queries, n, sim_search, mmr_search):
 def retrieve(queries, n):
     # Import local save of vector database #
     vectorstore = Chroma(persist_directory="db", embedding_function=model)
-    
+
     # Load in search algs
     sim_search = vectorstore.similarity_search_with_relevance_scores
     mmr_search = vectorstore.max_marginal_relevance_search
-    
+
     # Retrieve
     return search(queries, n, sim_search, mmr_search)
 
 
 # In[ ]:
-
-
-
-
